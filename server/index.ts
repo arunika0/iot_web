f6822db1591b64e99b71ef8b1f2a7f16ab8073ad
cakra <cakrapradiptakuntara2@gmail.com>
2025-06-12 15:40:42 +0700

    cr

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,21 @@ import { setupVite, serveStatic, log } from "./vite";
 import { initializeTimescaleDB } from "./init-timescale";
 
 const app = express();
+
+// CORS middleware for mobile app access
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
